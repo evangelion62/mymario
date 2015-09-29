143756ba4b1b1a4a1564a745d77c6eb92e89e0d1
sebastien <sebastien.ronger.evangelion@gmail.com>
2015-09-29 11:33:45 +0200

    set windows name
    definition du nom de la fenétre : "My mario"

--- Codeblocks/main.cpp
+++ Codeblocks/main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 
     // Window
 
-    window = SDL_CreateWindow("SDL 2.0 Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("My Mario", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
 
     if(!window)
     {
